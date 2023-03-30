436c9e4a21099436e240b6ddeabb2db37dce6e93
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-30 10:44:13 -0400

    fix: serverjs modified to handle unhandled rejections and to ensure graceful shutdown
    Signed-off-by: Adebayo Samuel <eladebayoor@gmail.com>

--- server.js
+++ server.js
@@ -1,20 +1,55 @@
-process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNCAUGHT EXCEPTION SHUTTING DOWN...');
-  process.exit(1);
-});
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './.config.env' });
 
-// Connect databases before app loads
-require('./db/db');
-require('./db/redis');
-
+const Logger = require('./helpers/logger');
+const db = require('./db/db');
+const redis = require('./db/redis');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const logger = new Logger('server');
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Function to handle graceful server shutdown
+async function shutdown(exitCode) {
+  try {
+    await db.close();
+  } catch (err) {
+    logger.error(`Error during MongoDB shutdown: ${err}`);
+  }
+
+  try {
+    await redis.close();
+  } catch (err) {
+    logger.error(`Error during Redis shutdown: ${err}`);
+  }
+
+  process.exit(exitCode);
+}
+
+(async () => {
+  try {
+    // Connect databases before app loads
+    await db.connect();
+    await redis.connect();
+
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+      logger.info(`Server listening on port ${port}`);
+    });
+  } catch (err) {
+    logger.error(`Error during server startup: ${err}`);
+    await shutdown(1);
+  }
+})();
+
+process.on('uncaughtException', async (err) => {
+  logger.error(`${err.name}: ${err.message}`);
+  logger.error('UNCAUGHT EXCEPTION SHUTTING DOWN...');
+  await shutdown(1);
+});
+
+process.on('unhandledRejection', async (err) => {
+  logger.error(`${err.name}: ${err.message}`);
+  logger.error('UNHANDLED REJECTION SHUTTING DOWN..');
+  await shutdown(1);
 });
